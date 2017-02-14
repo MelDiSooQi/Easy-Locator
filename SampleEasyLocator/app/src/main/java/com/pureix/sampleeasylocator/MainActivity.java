4f2e6f81717635506c86e74370ad8d64592f637b
Moataz Ibrahim El-DiSooQi <MelDiSooQi@gmail.com>
2017-02-14 22:47:44 +0200

    fix scroll in Sample

--- SampleEasyLocator/app/src/main/java/com/pureix/sampleeasylocator/MainActivity.java
+++ SampleEasyLocator/app/src/main/java/com/pureix/sampleeasylocator/MainActivity.java
@@ -99,6 +99,7 @@ public class MainActivity extends AppCompatActivity {
 
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setVisibility(View.GONE);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
